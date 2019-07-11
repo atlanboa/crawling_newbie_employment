422eb21a471b6d401a4e54d69cbfa4b53a7606e8
김상헌 <atlanboa@gmail.com>
2019-07-11 10:38:55 +0900

    day 2 crawls

--- app.py
+++ app.py
@@ -17,6 +17,136 @@ slack_events_adaptor = SlackEventAdapter(SLACK_SIGNING_SECRET, "/listening", app
 slack_web_client = WebClient(token=SLACK_TOKEN)
 
 
+# job info class
+class JobInfo:
+    def __init__(self, company, title, sub_title, career, education, location, employment_type, deadline, link):
+        self.company = company
+        self.title = title
+        self.sub_title = sub_title
+        self.career = career
+        self.education = education
+        self.location = location
+        self.employment_type = employment_type
+        self.deadline = deadline
+        self.link = link
+        self.co_link = 'http://www.jobkorea.co.kr/top100'
+
+    def show_info(self):
+        print('-------------------------------------------------------------------------')
+        print('회사명 ({})'.format(self.company))
+        print('메인 타이틀 ({})'.format(self.title))
+        print('서브 타이틀 ({})'.format(self.sub_title))
+        print('경력 구분 ({})'.format(self.career))
+        print('학력 ({})'.format(self.education))
+        print('지역 ({})'.format(self.location))
+        print('고용 형태 ({})'.format(self.employment_type))
+        print('마감 ({})'.format(self.deadline))
+        print('링크 ({})'.format(self.co_link+self.link))
+        print('-------------------------------------------------------------------------')
+
+    # setter
+    def __setitem__(self, key, value):
+        """Setting tag[key] sets the value of the 'key' attribute for the
+        tag."""
+        self.attrs[key] = value
+
+    # getter
+    def __getitem__(self, key):
+        """tag[key] returns the value of the 'key' attribute for the tag,
+        and throws an exception if it's not there."""
+        return self.attrs[key]
+
+
+# top 10 페이지 url
+def get_top10_url(task):
+    urls = {}
+    return urls[task]
+
+
+# top 10 페이지 크롤링
+def get_newbie_url(task):
+    urls = {}
+    return urls[task]
+
+
+def _crawl_newbie_info(task):
+    # url = get_newbie_url(task)
+    url = 'http://www.jobkorea.co.kr/Starter/?JoinPossible_Stat=0&schPart=%2C%2C10013%2C%2C&schOrderBy=0&LinkGubun=0&LinkNo=0&schType=0&schGid=0&Page=1'
+
+    source_code = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(source_code, "html.parser")
+
+    jobs = []
+    filter_list = soup.find('ul', class_='filterList')
+    for li in filter_list.find_all('li'):
+        company = li.find('a', class_='coLink').getText()
+        title = li.find('a', class_='link').getText()
+        sub_title = li.find('div', class_='sTit').getText()
+        sdsc = li.find('div', class_='sDesc')
+        employment_type = sdsc.find('strong').getText()
+        career = ''
+        try:
+            education = sdsc.find_all('span')[0].getText()
+            location = sdsc.find_all('span')[1].getText()
+
+        except IndexError:
+            pass
+        deadline = li.find('span', class_='day').getText()
+        link = li.find('a', class_='coLink').get('href')
+        job = JobInfo(company,
+                      title,
+                      sub_title,
+                      career,
+                      education,
+                      location,
+                      employment_type,
+                      deadline,
+                      link
+        )
+        job.show_info()
+        jobs.append(job)
+        return jobs
+
+
+# top 10 페이지 크롤링
+def _crawl_job_info(task):
+    url = get_top10_url(task)
+    source_code = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(source_code, "html.parser")
+
+    jobs = []
+    rank_list = soup.find('ol', class_='rankList')
+    for li in rank_list.find_all('li'):
+        company = li.find('a', class_='coLink').getText()
+        title = li.find('a', class_='link').getText()
+        sub_title = li.find('div', class_='sTit').getText()
+        sdsc = li.find('div', class_='sDsc')
+        try:
+            career = sdsc.find_all('span')[0].getText()
+            education = sdsc.find_all('span')[1].getText()
+            location = sdsc.find_all('span')[2].getText()
+            employment_type = sdsc.find_all('span')[3].getText()
+        except IndexError:
+            pass
+        deadline = li.find('span', class_='day').getText()
+        link = li.find('a', class_='coLink').get('href')
+        job = JobInfo(company,
+                      title,
+                      sub_title,
+                      career,
+                      education,
+                      location,
+                      employment_type,
+                      deadline,
+                      link
+        )
+        jobs.append(job)
+        # job.show_info()
+    return jobs
+
+
+
+
 def job_korea():
     # URL 데이터를 가져올 사이트 url 입력
     url = "http://www.jobkorea.co.kr/top100/"
@@ -83,7 +213,7 @@ def index():
     return "<h1>Server is ready.</h1>"
 
 
-
 if __name__ == '__main__':
-    job_korea()
-    app.run('127.0.0.1', port=5000)
+    # _crawl_job_info('text')
+    _crawl_newbie_info('task')
+    # app.run('127.0.0.1', port=5000)
