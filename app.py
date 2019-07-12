9dce3c7c0bb4d347d247a8c11c80b598a0b9f821
김상헌 <atlanboa@gmail.com>
2019-07-12 10:22:50 +0900

    07/12/10:22

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
-
-from flask import Flask
+import json
+from flask import Flask, request
 from slack import WebClient
-from slackeventsapi import SlackEventAdapter
 import chat_with_my_bot
 import make_block
+from slack.web.classes.blocks import *
+from slack.web.classes.elements import *
+from slack.web.classes.interactions import MessageInteractiveEvent
+from slackeventsapi import SlackEventAdapter
 
 SLACK_TOKEN = 'xoxb-691797361766-689184678356-0H4RfMhRMctD17vM3qd4awhn'
 SLACK_SIGNING_SECRET = '56c25316dd82cc632339a6dc295701d3'
@@ -15,6 +18,9 @@ slack_events_adaptor = SlackEventAdapter(SLACK_SIGNING_SECRET, "/listening", app
 slack_web_client = WebClient(token=SLACK_TOKEN)
 
 
+job_objects = []
+
+
 # 챗봇이 멘션을 받았을 경우 (이벤트 처리 부분)
 @slack_events_adaptor.on("app_mention")
 def app_mentioned(event_data):
@@ -22,14 +28,16 @@ def app_mentioned(event_data):
     text = event_data["event"]["text"]
 
     # chat_with_my_bot._chat_with_mybot()
-
+    global job_objects
     if text == '<@UL9K54M32>':
         keywords = '안녕나는 챗봇이얌~~!! 취업정보를 알려주는 봇이얌 ^_^'
     else:
+        # job_objects 는 blocks
         job_objects = chat_with_my_bot._chat_with_mybot(text)
+
         slack_web_client.chat_postMessage(
             channel=channel,
-            blocks=make_block.make_block(job_objects)
+            blocks=extract_json(make_block.make_block(job_objects, 5))
         )
     # if type(keywords) == str:
     #     slack_web_client.chat_postMessage(
@@ -53,6 +61,34 @@ def app_mentioned(event_data):
 @app.route("/", methods=["GET"])
 def index():
     return "<h1>Server is ready.</h1>"
+
+
+current = 5
+
+
+@app.route("/click", methods=["GET", "POST"])
+def on_button_click():
+    # 버튼 클릭은 SlackEventsApi에서 처리해주지 않으므로 직접 처리합니다
+
+    payload = request.values["payload"]
+    click_event = MessageInteractiveEvent(json.loads(payload))
+
+    global current
+    # keyword = click_event.block_id
+    current = int(click_event.value)
+
+    # 다른 리스트로 블록을 재가공
+    message_blocks = make_block.make_block(job_objects, current)
+
+    # 메시지를 채널에 올립니다
+    slack_web_client.chat_postMessage(
+        channel=click_event.channel.id,
+        blocks=extract_json(message_blocks)
+    )
+
+    # Slack에게 클릭 이벤트를 확인했다고 알려줍니다
+    return "OK", 200
+
 
 
 if __name__ == '__main__':
--- make_block.py
+++ make_block.py
@@ -1,68 +1,49 @@
-from slack.web.classes import extract_json
 from slack.web.classes.blocks import *
+from slack.web.classes.elements import *
 
 
-def make_block(_jobs):
-    head_block = [{
-        "type": "section",
-        "text": {
-            "type": "mrkdwn",
-            "text": "*There are many companies for you, Check it out, Take your wings*"
-        }
-    }]
+def make_block(_jobs, current):
+
     blocks = []
 
-    index = 1
-    for _job in _jobs:
-        if index < 5:
-            index += 1
-            company = _job.__getattribute__('company')
-            title = _job.__getattribute__('title')
-            sub_title = _job.__getattribute__('sub_title')
-            career = _job.__getattribute__('career')
-            education = _job.__getattribute__('education')
-            location = _job.__getattribute__('location')
-            employment_type = _job.__getattribute__('employment_type')
-            deadline = _job.__getattribute__('deadline')
-            link = _job.__getattribute__('link')
+    for i in range(current-5, current):
+        company = _jobs[i].__getattribute__('company')
+        title = _jobs[i].__getattribute__('title')
+        sub_title = _jobs[i].__getattribute__('sub_title')
+        career = _jobs[i].__getattribute__('career')
+        education = _jobs[i].__getattribute__('education')
+        location = _jobs[i].__getattribute__('location')
+        employment_type = _jobs[i].__getattribute__('employment_type')
+        deadline = _jobs[i].__getattribute__('deadline')
+        link = _jobs[i].__getattribute__('link')
 
-            block = SectionBlock(
-                text="*<" + link + " | " + company + ">*\n" + sub_title + '\n경력 : ' + career +
-                     " | 학력 : " + education + " | 지역 : " + location + " | 고용 형태 : " + employment_type +
-                     " | 마감일 : " + deadline
-            )
-            blocks.append(block)
-        else:
-            break
-    # divider = DividerBlock(
-    #     subtype="divider"
-    # )
+        block = SectionBlock(
+            text="*<" + link + " | " + company + ">*\n" + sub_title + '\n경력 : ' + career +
+                 " | 학력 : " + education + " | 지역 : " + location + " | 고용 형태 : " + employment_type +
+                 " | 마감일 : " + deadline
+        )
+        blocks.append(block)
 
-    # 섹션 블록: 짧은 문구 여러 개를 2줄로 표시합니다 (최대 10개).
-    block2 = SectionBlock(
-        fields=["text1", "text2", "text1", "text2"]
+    button_actions = ActionsBlock(
+        block_id='button',
+        elements=[
+            ButtonElement(
+                text="이전 5개",
+                action_id="last_5", value=str(current - 5)
+            ),
+            ButtonElement(
+                text="다음 5개",
+                action_id="next_5", value=str(current + 5)
+            ),
+        ]
     )
 
-    # 이미지 블록: 큰 이미지 하나를 표시합니다..
-    block3 = ImageBlock(
-        image_url="이미지의 URL",
-        alt_text="이미지가 안 보일 때 대신 표시할 텍스트"
-    )
+    blocks.append(button_actions)
+
+    return blocks
 
 
 
-
-
-
-
-    # # 여러 개의 블록을 하나의 메시지로 묶어 보냅니다.
-    # my_blocks = [block1, block2, block3]
-    # slack_web_client.chat_postMessage(
-    #     channel="#채널명",
-    #     blocks=extract_json(my_blocks)
-    # )
-    #
-    return extract_json(blocks)
 
 
 
